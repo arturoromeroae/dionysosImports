58074a1cb95f7e256838440bf1a81fec87253613
Arturo Romero <aromero@aeonlinesolutions.com>
2022-04-02 19:51:46 -0500

    fix(env)

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
 
-fs.writeFileSync('./.env', `CUSTOM_URL=${process.env.CUSTOM_URL}\n`);
+fs.writeFileSync('./.env', `CUSTOM_URL=${process.env.CUSTOM_URL}\n API=${process.env.API}`);
