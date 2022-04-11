9e43655a48191e9d8c5168e8b4bc381193eb7f08
Arturo Romero <aromero@aeonlinesolutions.com>
2022-04-11 00:01:03 -0500

    se mejora la pagina de contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,39 +2,158 @@ import React from 'react';
 import '@styles/ContactPage.scss'
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import Banner from '@components/Banner';
+import contactBanner from '@images/Banner.jpg';
 import logo from '@images/dionysos-logo.png'
 
 const Contact = () => {
   return (
     <>
         <Header />
+        <Banner srcBanner={contactBanner} title='Contact Us' />
         <div className='contactContainer'>
-            <div>
-                <h1 className='contactTitle'>Contact Us</h1>
-                <div className='contactContainerForm'>
-                    <div className='contactForm'>
-                        <div>
-                            <img src={logo} />
-                            <div>
-                                <p>11581 Robertson Dr Manassas, VA 2010.</p>
-                                <p>Tel: (703) 392-7073</p>
-                                <p>Monday – Friday: 8am – 5pm</p>
-                            </div>
-                            <div></div>
-                        </div>
-                        <form>
-                            <input type="text" />
-                            <input type="text" />
-                            <input type="text" />
-                            <div>
-                                <button>Reset</button>
-                                <button>Submit</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div className='contactMap'>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12444.72187412986!2d-77.55386777589084!3d38.759558687836105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b65d95152804b1%3A0x782fbc854595fa07!2sDionysos%20Imports%2C%20Inc.!5e0!3m2!1sen!2spe!4v1649220545555!5m2!1sen!2spe" width="519" height="326" style={{borderRadius: 20, border: 0}} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
+            <div className='addressContainer'>
+                <img src={logo} />
+                <div>
+                    <h2>Dionysos Imports, Inc.</h2>
+                    <p>11581 Robertson Dr,</p>
+                    <p>Manassas, VA 20109</p>
+                    <p><strong>Tel:</strong> (703) 392-7073</p>
+                    <p><strong>Fax:</strong> (703) 392-7075</p>
+                </div>
+            </div>
+            <div className='contactFormContainer'>
+                <form className='contactForm'>
+                    <label>Your Name</label>
+                    <br />
+                    <input type="text" />
+                    <br />
+                    <label>Your Email</label>
+                    <br />
+                    <input type="email" />
+                    <br />
+                    <label>Your Message</label>
+                    <br />
+                    <textarea />
+                    <br />
+                    <button>Send</button>
+                </form>
+            </div>
+            <div className='contactEmployees'>
+                <div>
+                    <h3>President</h3>
+                    <p>Yannis Tsapos</p>
+                    <p>yannis.tsapos@dionysosimports.com</p>
+                </div>
+                <div>
+                    <h3>Office Manager</h3>
+                    <p>David Costa</p>
+                    <p>david.costa@dionysosimports.com</p>
+                </div>
+            </div>
+            <div className='contactEmployees'>
+                <div>
+                    <h3>Business Development</h3>
+                    <p>John Anastasakis</p>
+                    <p>john.anastasakis@dionysosimports.com</p>
+                </div>
+                <div>
+                    <h3>Wine Manager</h3>
+                    <p>Catherine Kaylor</p>
+                    <p>catherine.kaylor@dionysosimports.com</p>
+                </div>
+            </div>
+            <h2 className='contactTitle'>Sales Representatives</h2>
+            <div className='contactEmployees'>
+                <div>
+                    <h3>Maryland</h3>
+                    <p>Alexandros Georgiadis</p>
+                    <p><a href='301-518-1935'>(301) 518-1935</a></p>
+                    <p>alexgeorgiadis89@gmail.com</p>
+                </div>
+                <div>
+                    <h3>Northern Virginia</h3>
+                    <p>Amy Evans</p>
+                    <p><a href="tel:202-321-6387">(202) 321-6387</a></p>
+                    <p>amy.evans@dionysosimports.com</p>
+                </div>
+            </div>
+            <div className='contactEmployees'>
+                <div>
+                    <h3>Northern Virginia, Washington DC, Maryland</h3>
+                    <p>Catherine Kaylor</p>
+                    <p><a href='tel:571-437-4490'>(571) 437-4490</a></p>
+                    <p>catherine.kaylor@dionysosimports.com</p>
+                </div>
+                <div>
+                    <h3>Richmond, Fredericksburg, Charlottesville</h3>
+                    <p>Clinton Emerson</p>
+                    <p><a href="tel:804-304-6231">(804) 304-6231</a></p>
+                    <p>clinton.emerson@dionysosimports.com</p>
+                </div>
+            </div>
+            <div className='contactEmployees'>
+                <div>
+                    <h3>Northern Virginia, Washington DC</h3>
+                    <p>Dan Metzger</p>
+                    <p><a href='tel:540-613-0671'>(540) 613-0671</a></p>
+                    <p>dan.metzger@dionysosimports.com</p>
+                </div>
+                <div>
+                    <h3>Montgomery County, Maryland</h3>
+                    <p>Demetria Loukas</p>
+                    <p><a href="tel:240-731-6372">(240) 731-6372</a></p>
+                    <p>demetria.loukas@dionysosimports.com</p>
+                </div>
+            </div>
+            <div className='contactEmployees'>
+                <div>
+                    <h3>Northern Virginia</h3>
+                    <p>Fernanzo Leonzo</p>
+                    <p><a href='tel:703-906-7855'>(703) 906-7855</a></p>
+                    <p>fernando.leonzo@dionysosimports.com</p>
+                </div>
+                <div>
+                    <h3>Richmond, Charlottesville, VA Beach, Norfolk, Williamsburg</h3>
+                    <p>Genevelyn Steele</p>
+                    <p><a href="tel:804-426-8089">(804) 426-8089</a></p>
+                    <p>genevelyn.steele@dionysosimports.com</p>
+                </div>
+            </div>
+            <div className='contactEmployees'>
+                <div>
+                    <h3>Northern Virginia, Washington DC</h3>
+                    <p>Matt Perrella</p>
+                    <p><a href='tel:954-529-0908'>(954) 529-0908</a></p>
+                    <p>matt.perrella@dionysosimports.com</p>
+                </div>
+                <div>
+                    <h3>Montgomery County</h3>
+                    <p>Nazila Safari</p>
+                    <p><a href="tel:202-812-5866">(202) 812-5866</a></p>
+                    <p>nazila.safari@dionysosimports.com</p>
+                </div>
+            </div>
+            <div className='contactEmployees'>
+                <div>
+                    <h3>VA Beach, Newport News, Norfolk</h3>
+                    <p>Seth White</p>
+                    <p><a href='tel:757-749-5377'>(757) 749-5377</a></p>
+                    <p>seth.white@dionysosimports.com</p>
+                </div>
+                <div>
+                    <h3>Washington DC</h3>
+                    <p>Thomas Valis</p>
+                    <p><a href="tel:240-305-6337">(240) 305-6337</a></p>
+                    <p>thomasvalis@yahoo.com</p>
+                </div>
+            </div>
+            <div className='contactEmployees'>
+                <div>
+                    <h3>Northern Virginia, Washington DC</h3>
+                    <p>Zdravko Solenkov</p>
+                    <p><a href='tel:571-426-8277'>(571) 426-8277</a></p>
+                    <p>zdravko.solenkov@dionysosimports.com</p>
                 </div>
             </div>
         </div>
